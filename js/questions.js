3b93f6939cd53139ab9d79c07e124e7b0f2ea454
Brian Flieck <Brianflieck@gmail.com>
2021-01-17 13:55:30 -0500

    updates to JS file, added extra questions, updated CSS with some button css snippets

--- js/questions.js
+++ js/questions.js
@@ -1,18 +1,53 @@
-var questionsToAsk = [ 
+var questionsToAsk = [
   {
-      question: "what is an array used for?",
-      multipleChoice: ["store multiple values in a single variable", "store images in a single variable", "A  guy named r-a-y"],
-      answer: "store multiple values in a single variable"
+    question: "what is an array used for?",
+    multipleChoice: ["store multiple values in a single variable", "store images in a single variable", "A  guy named r-a-y"],
+    answer: "store multiple values in a single variable"
   },
   {
-      question: "What is bootstrap?",
-      multipleChoice: ["dockmartin laces", "CSS Framework for developing responsive and mobile-first websites", "css wizardary"],
-      answer: "CSS Framework for developing responsive and mobile-first websites"
+    question: "What is bootstrap?",
+    multipleChoice: ["dockmartin laces", "CSS Framework for developing responsive and mobile-first websites", "css wizardary"],
+    answer: "CSS Framework for developing responsive and mobile-first websites"
   },
   {
     question: "What is HTML?",
     multipleChoice: ["Hyper Text Markup Language", "Hot Tots Molten Lava", "Hyper Text Magic Langague"],
     answer: "Hyper Text Markup Language"
-}
+  },
+  {
+    question: "What does DOM stand for??",
+    multipleChoice: ["Document Object Method", "Dos Object Method", "Document Object Method"],
+    answer: "Document Object Method"
+  },
+  {
+    question: "question 5 ",
+    multipleChoice: ["-2", "5", "-3"],
+    answer: "5"
+  },
+  {
+    question: "question 6 ",
+    multipleChoice: ["-2", "6", "-3"],
+    answer: "6"
+  },
+  {
+    question: "question 7 ",
+    multipleChoice: ["-2", "7", "-3"],
+    answer: "7"
+  },
+  {
+    question: "question 8 ",
+    multipleChoice: ["-2", "8", "-3"],
+    answer: "8"
+  },
+  {
+    question: "question 9 ",
+    multipleChoice: ["-2", "9", "-3"],
+    answer: "9"
+  },
+  {
+    question: "question 10 ",
+    multipleChoice: ["-2", "10", "-3"],
+    answer: "10"
+  }
 ];
 
--- js/script.js
+++ js/script.js
@@ -1,68 +1,114 @@
 let highscore = document.getElementById("#highscore");
+let endGame = document.getElementById("endQuizGreeting");
 let multipleChoiceButtons = document.getElementById("#multipleChoiceButtons");
 let askQuestionsDisplay = document.getElementById("#questions");
 let CountDownTimer = document.getElementById("#timer");
 let instructions = document.getElementById("#instructions");
 let startButton = document.getElementById("#start");
+
 let buttonA = document.getElementById("#a");
 let buttonB = document.getElementById("#b");
 let buttonC = document.getElementById("#c");
-var secondsLeft = 90;
-// var score = '';
-var currentQuestion = 0;
 
+let highScoresArchive = document.getElementById("UserHighScoreArchive");
+let username = document.getElementById("userName");
+let userNameScore = document.getElementById("userNameScore");
+
+var secondsLeft = 90;
+let score = 0;
+//using this later for display of the 
+var highscoreUser = localStorage.getItem("score");
+var currentQuestion = 0;
+//make question area larger text!
+askQuestionsDisplay.style.fontSize = "30px";
+endGame.style.display = "none";
+highScoresArchive.style.display = "none";
 
 startButton.addEventListener("click", startQuiz);
 
-function startQuiz () {
-setTime();
-askingQuestions();
+//start quiz 
+function startQuiz() {
+  setTime();
+  askingQuestions();
 };
 
 
 
 function setTime() {
-    // Sets interval in variable
-    // hide the instructions portion of quiz
-    instructions.style.visibility = "hidden";
+  // Sets interval in variable
+  // hide the instructions portion of quiz
+  instructions.style.display = "none";
 
-    var timerInterval = setInterval(function() {
-      secondsLeft--;
-      CountDownTimer.textContent = "time remaining: " + secondsLeft;
-  
-      if(currentQuestion === questionsToAsk.length) {
-        clearInterval(timerInterval);
-      }else{
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    CountDownTimer.textContent = "time remaining: " + secondsLeft;
 
-      }
-  
-    }, 1000);
-  }
-
-  function askingQuestions () {
-    if (currentQuestion === questionsToAsk.length){
-        // askQuestionsDisplay.style.visibility = "hidden";
+    if (secondsLeft <= 0) {
+      clearInterval(timerInterval);
 
     }
-askQuestionsDisplay.textContent = questionsToAsk[currentQuestion].question;
-buttonA.textContent = questionsToAsk[currentQuestion].multipleChoice[0];
-buttonB.textContent = questionsToAsk[currentQuestion].multipleChoice[1];
-buttonC.textContent = questionsToAsk[currentQuestion].multipleChoice[2];
+
+  }, 1000);
+}
+
+function askingQuestions() {
+  if (currentQuestion === questionsToAsk.length) {
+
+
+  }
+  askQuestionsDisplay.textContent = questionsToAsk[currentQuestion].question;
+  buttonA.textContent = questionsToAsk[currentQuestion].multipleChoice[0];
+  buttonB.textContent = questionsToAsk[currentQuestion].multipleChoice[1];
+  buttonC.textContent = questionsToAsk[currentQuestion].multipleChoice[2];
 
 };
 
+
+
+
+//multiple choice section
 multipleChoiceButtons.addEventListener("click", function (event) {
-    if (event.target.matches("button")) {
-        if (event.target.textContent === questionsToAsk[currentQuestion].answer) {
-            currentQuestion++;
+  if (event.target.matches("button")) {
+    if (event.target.textContent === questionsToAsk[currentQuestion].answer) {
+      currentQuestion++;
+      score++;
+      highscore.textContent = "View Highscores: " + score;
+      console.log(score);
 
-        } else {
-            currentQuestion++;
-            secondsLeft = secondsLeft - 10;
 
-        }
-        askingQuestions();
+    } else {
+      currentQuestion++;
+      secondsLeft = secondsLeft - 10;
+
     }
+    askingQuestions();
+  
+    
+  }
 });
 
 
+highscore.addEventListener("click", function (event) {
+  if (endQuizGreeting.style.display === "none") {
+    endQuizGreeting.style.display = "block";
+    UserHighScoreArchive.style.display = "block";
+      // storePlayerInfo();
+  } else {
+    endQuizGreeting.style.display = "none";
+    UserHighScoreArchive.style.display = "none";
+
+  }
+})
+//keep score secton
+// function scoreKeeper() {
+//   if (score > 0) {
+
+//     // localStorage.setItem("highscore", score);
+//   }
+// }
+
+  function endOfGame() {
+    endQuizGreeting.style.display = "block";
+    UserHighScoreArchive.style.display = "block";
+
+  }
